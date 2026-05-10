9e1214b853ef0142900dc7af7e67dc7235a4fb21
Bandaru srinivas <bandarusrinivas626@gmail.com>
2026-05-10 20:41:08 +0530

    learned input box

--- Bandaru_React/src/Event_Handling/OnClick_passingAurg.jsx
+++ Bandaru_React/src/Event_Handling/OnClick_passingAurg.jsx
@@ -2,16 +2,32 @@ import React from 'react'
 import { useState } from 'react'
 const OnClick_passingAurg = () => {
     const [color,setColor]=useState("");
+    const [name,Setname]=useState("")
     const handleClick=(color)=>{
         console.log(color)
         setColor(color);
     }
+
+    const handleInput=(e)=>{
+        console.log(`Hi ${name}`)
+        Setname(e.target.value);
+    }
+
+    const ShowName =()=>{
+        console.log(`Hi ${name}`)
+    }
+
   return (
     <div>
       <button onClick={()=>{handleClick("Red")}}>Red</button>
       <button onClick={()=>{handleClick("Green")}}>Green</button>
       <h1>{color}</h1>
+
+      <input type="text" onChange={handleInput} />
+      <button onClick={ShowName}>click to see name</button> 
+        <h1>{name}</h1>
     </div>
+    
   )
 }
 
