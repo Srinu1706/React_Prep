c94fadfc6c0a9950c4c7c8b7539b9c8bf7aed387
Bandaru srinivas <bandarusrinivas626@gmail.com>
2026-05-10 20:07:47 +0530

    passed aurgments to the functions and learned  onClick

---
+++ Bandaru_React/src/Event_Handling/OnClick_passingAurg.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useState } from 'react'
+const OnClick_passingAurg = () => {
+    const [color,setColor]=useState("");
+    const handleClick=(color)=>{
+        console.log(color)
+        setColor(color);
+    }
+  return (
+    <div>
+      <button onClick={()=>{handleClick("Red")}}>Red</button>
+      <button onClick={()=>{handleClick("Green")}}>Green</button>
+      <h1>{color}</h1>
+    </div>
+  )
+}
+
+export default OnClick_passingAurg
+// <button onClick={handleClick("Red")}> then function runs immediately when page loads. To avoid this we use arrow function and pass the function as a callback.
--- Bandaru_React/src/main.jsx
+++ Bandaru_React/src/main.jsx
@@ -4,11 +4,13 @@ import './index.css'
 import App from './App.jsx'
 import Home from './Assignment2/Home.jsx'
 import OnClick from './Event_Handling/OnClick.jsx'
+import OnClick_passingAurg from './Event_Handling/OnClick_passingAurg.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
     {/* <Home /> */}
-    <OnClick />
+    {/* <OnClick /> */}
+    <OnClick_passingAurg />
   </StrictMode>,
 )
